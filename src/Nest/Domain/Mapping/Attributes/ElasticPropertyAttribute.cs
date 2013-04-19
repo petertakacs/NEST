ec3677f6fb6e7c2b45832ca27e40643c9b0b7d21
peter.takacs <takacs.peter@encosoft.hu>
2013-04-19 12:49:29 +0200

    Added IndexOptions property to IElasticPropertyAttribute

--- src/Nest/Domain/Mapping/Attributes/ElasticPropertyAttribute.cs
+++ src/Nest/Domain/Mapping/Attributes/ElasticPropertyAttribute.cs
@@ -2,58 +2,59 @@
 
 namespace Nest
 {
-	[AttributeUsage(System.AttributeTargets.Property, AllowMultiple = false)]
-	public class ElasticPropertyAttribute : Attribute, IElasticPropertyAttribute
-	{
-		public bool AddSortField { get; set; }
+    [AttributeUsage(System.AttributeTargets.Property, AllowMultiple = false)]
+    public class ElasticPropertyAttribute : Attribute, IElasticPropertyAttribute
+    {
+        public bool AddSortField { get; set; }
 
-		public bool OptOut { get; set; }
-		
-		public string Name { get; set; }
+        public bool OptOut { get; set; }
 
-		public FieldType Type { get; set; }
+        public string Name { get; set; }
 
-		public TermVectorOption TermVector { get; set; }
-		public FieldIndexOption Index { get; set; }
+        public FieldType Type { get; set; }
 
-		public double Boost { get; set; }
+        public TermVectorOption TermVector { get; set; }
+        public FieldIndexOption Index { get; set; }
 
-		public string Analyzer { get; set; }
-		public string IndexAnalyzer { get; set; }
-		public string SearchAnalyzer { get; set; }
-		public string NullValue { get; set; }
-    public string Similarity { get; set; }
+        public double Boost { get; set; }
 
-		public bool OmitNorms { get; set; }
-		public bool OmitTermFrequencyAndPositions { get; set; }
-		public bool IncludeInAll { get; set; }
-		public bool Store { get; set; }
+        public string Analyzer { get; set; }
+        public string IndexAnalyzer { get; set; }
+        public string SearchAnalyzer { get; set; }
+        public string NullValue { get; set; }
+        public string Similarity { get; set; }
 
-		/// <summary>
-		/// Defaults to float so be sure to set this correctly!
-		/// </summary>
-		public NumericType NumericType { get; set; }
-		public int PrecisionStep { get; set; }
+        public bool OmitNorms { get; set; }
+        public bool OmitTermFrequencyAndPositions { get; set; }
+        public IndexOptions? IndexOptions { get; set; }
+        public bool IncludeInAll { get; set; }
+        public bool Store { get; set; }
 
-		/// <summary>
-		/// http://www.elasticsearch.org/guide/reference/mapping/date-format.html
-		/// </summary>
-		public string DateFormat { get; set; }
+        /// <summary>
+        /// Defaults to float so be sure to set this correctly!
+        /// </summary>
+        public NumericType NumericType { get; set; }
+        public int PrecisionStep { get; set; }
 
-		public ElasticPropertyAttribute()
-		{
-			//make sure we match ES's defaults
-			this.Boost = 1;
-			this.TermVector = TermVectorOption.no; 
-			this.Index = FieldIndexOption.analyzed;
-		
-			this.IncludeInAll = true;
-			this.PrecisionStep = 4;
-		}
+        /// <summary>
+        /// http://www.elasticsearch.org/guide/reference/mapping/date-format.html
+        /// </summary>
+        public string DateFormat { get; set; }
+
+        public ElasticPropertyAttribute()
+        {
+            //make sure we match ES's defaults
+            this.Boost = 1;
+            this.TermVector = TermVectorOption.no;
+            this.Index = FieldIndexOption.analyzed;
+
+            this.IncludeInAll = true;
+            this.PrecisionStep = 4;
+        }
 
         public void Accept(IElasticPropertyVisitor visitor)
         {
             visitor.Visit(this);
         }
-	}
+    }
 }
